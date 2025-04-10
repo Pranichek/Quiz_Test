85db4bed715743ca0dca5a49c1c2319c77df50e3
admin <vovagrinchenko19@gmail.com>
2025-04-10 17:35:50 +0300

    connect css file to html

--- Project/urls.py
+++ Project/urls.py
@@ -2,6 +2,6 @@ import home_app
 
 #
 home_app.home.add_url_rule(
-    rule= "/home", #
+    rule = "/", #/ - головна стоірнка
     view_func= home_app.render_home #
 )
--- home_app/app.py
+++ home_app/app.py
@@ -1,10 +1,9 @@
 import flask
 
-#
 home = flask.Blueprint(
-    name = "home", # 
-    import_name = "home_app", #
-    static_url_path = "/static/", #
-    static_folder = "static", #
-    template_folder = "templates" #
+    name = "home",  
+    import_name = "home_app", 
+    static_url_path = "/home/static/", 
+    static_folder = "static", 
+    template_folder = "templates" 
 )
